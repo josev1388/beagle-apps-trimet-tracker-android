5a4c500a7909b99afad0c6c94964bf9b67f1fa93
joe.pinsonault@gmail.com <joe.pinsonault@gmail.com@28dffc3e-4a99-678d-ebd2-6d9ea7f14789>
2011-08-18 03:15:18 +0000

    Changed the top and bottom bars to look a little nicer.

--- com.beagleapps.android.trimettracker/src/com/beagleapps/android/trimettracker/homepage.java
+++ com.beagleapps.android.trimettracker/src/com/beagleapps/android/trimettracker/homepage.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 
 import android.app.Activity;
 import android.app.ProgressDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.AsyncTask;
@@ -24,7 +23,8 @@ import android.widget.Toast;
 public class homepage extends Activity {
 	/** Called when the activity is first created. */
 
-	private String TAG = "homepage";
+	@SuppressWarnings("unused")
+	private static String TAG = "homepage";
 
 	private ArrayList<Favorite> mFavorites = null;
 	private FavoriteAdapter favoriteAdapter;
@@ -34,8 +34,11 @@ public class homepage extends Activity {
 	private TextView stopIDTextBox;
 	private Button goButton;
 
-	private static ArrivalsDocument mXmlArrivalsDoc;
+	private static ArrivalsDocument mArrivalsDoc;
 	ArrayAdapter<String> listViewAdapter;
+	
+	private DownloadArrivalData mDownloadTask = null;
+	private ProgressDialog mDialog;
 
 
 	@Override
@@ -75,7 +78,32 @@ public class homepage extends Activity {
 				onStopClick(mFavorites.get(position).getStopID());
 			}
 		});
+		
+		setupDialog();
+		mDownloadTask = (DownloadArrivalData)getLastNonConfigurationInstance();
+		
+		if (mDownloadTask != null){
+			mDownloadTask.attach(this);
+			if (mDownloadTask.isDone()){
+				dismissDialog();
+			}
+			else{
+				showDialog();
+			}
+		}
 
+	}
+	
+	@Override 
+	protected void onPause(){
+		super.onPause();
+		//mDbHelper.close();
+	}
+	
+	@Override 
+	protected void onDestroy(){
+		super.onDestroy();
+		//mDbHelper.close();
 	}
 
 	public void onWindowFocusChanged (boolean hasFocus){
@@ -91,7 +119,8 @@ public class homepage extends Activity {
 
 		if (Connectivity.checkForInternetConnection(getApplicationContext()))
 		{
-			new DownloadArrivalData().execute(urlString);
+			mDownloadTask = new DownloadArrivalData(this);
+			mDownloadTask.execute(urlString);
 		}
 		else{
 			Connectivity.showErrorToast(getApplicationContext());
@@ -135,23 +164,60 @@ public class homepage extends Activity {
 
 
 	public void setXmlArrivalsDoc(ArrivalsDocument xmlArrivalsDoc) {
-		homepage.mXmlArrivalsDoc = xmlArrivalsDoc;
+		homepage.mArrivalsDoc = xmlArrivalsDoc;
 	}
 
 
 	public ArrivalsDocument getXmlArrivalsDoc() {
-		return mXmlArrivalsDoc;
+		return mArrivalsDoc;
+	}
+	
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		if (mDownloadTask != null){
+			mDownloadTask.detach();
+		}
+		
+		return(mDownloadTask);
+	}
+	
+	private void setupDialog(){
+		mDialog = new ProgressDialog(this);
+		mDialog.setMessage(getString(R.string.dialogGettingArrivals));
+		mDialog.setIndeterminate(true);
+		mDialog.setCancelable(false);
 	}
 
-	private Context getDialogContext() {
-		Context context;
-		if (getParent() != null) context = getParent();
-		else context = this;
-		return context;
+	private void showDialog(){
+		mDialog.show();
 	}
 
-	private class DownloadArrivalData extends AsyncTask<String, Void, XMLHandler> {
-		private ProgressDialog dialog = new ProgressDialog(getDialogContext());
+	private void dismissDialog(){
+		mDialog.dismiss();
+	}
+
+	private static class DownloadArrivalData extends AsyncTask<String, Void, XMLHandler> {
+		private homepage activity = null;
+		private boolean isDone = false;
+		private final String TAG = "DownloadArrivalData asyncTask";
+
+		DownloadArrivalData(homepage activity) {
+			attach(activity);
+		}
+
+		public boolean isDone() {
+			return isDone;
+		}
+
+		private void attach(homepage activity) {
+			this.activity = activity;
+
+		}
+
+		private void detach() {
+			activity = null;
+
+		}
 
 		protected XMLHandler doInBackground(String... urls) {
 			XMLHandler newXmlHandler = null;
@@ -165,25 +231,33 @@ public class homepage extends Activity {
 		}
 
 		protected void onPreExecute() {
-			dialog.setMessage(getString(R.string.dialogGettingArrivals));
-			dialog.setIndeterminate(true);
-			dialog.setCancelable(false);
-			dialog.show();
+			isDone = false;
+			
+			if (activity != null){
+				activity.showDialog();
+			}
+			else{
+				Log.w(TAG, "showStop activity is null");
+			}
 		}
 
 		protected void onPostExecute(XMLHandler newXmlHandler) {
-			dialog.dismiss();
-
-			mXmlArrivalsDoc = new ArrivalsDocument(newXmlHandler.getXmlDoc(),
-					newXmlHandler.getRequestTime());
-
-			if (newXmlHandler.hasError())
-				showError(newXmlHandler.getError());
+			isDone = true;
+			
+			if (activity != null){
+				activity.dismissDialog();
+				if (newXmlHandler.hasError())
+					activity.showError(newXmlHandler.getError());
+				else{
+					ArrivalsDocument.mXMLDoc = newXmlHandler.getXmlDoc();
+					ArrivalsDocument.mRequestTime = newXmlHandler.getRequestTime();
+					
+					activity.showStop();
+				}
+			}
 			else{
-				showStop();
+				Log.w(TAG, "showStop activity is null");
 			}
 		}
 	}
-
-
 }
